f4e0a6b0757596ae8b24ea45855bd46a03bd39e0
Chrysan Angela Piarso <chrysan.angela@gmail.com>
2024-02-15 16:00:34 +0900

    feat: add typing

--- thanosql/__init__.py
+++ thanosql/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-API_TOKEN = os.environ.get("API_TOKEN", "")
-ENGINE_URL = os.environ.get("ENGINE_URL", "")
+API_TOKEN: str = os.environ.get("API_TOKEN", "")
+ENGINE_URL: str = os.environ.get("ENGINE_URL", "")
--- thanosql/_client.py
+++ thanosql/_client.py
@@ -3,7 +3,10 @@ from thanosql._base_client import ThanoSQLBaseClient
 
 
 class ThanoSQL(ThanoSQLBaseClient):
-    def __init__(self, api_token=API_TOKEN, engine_url=ENGINE_URL):
+    api_token: str
+    engine_url: str
+    
+    def __init__(self, api_token: str = API_TOKEN, engine_url: str = ENGINE_URL) -> None:
         super().__init__()
         
         self.api_token = api_token
