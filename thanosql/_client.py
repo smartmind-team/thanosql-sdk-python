f159762900df32045693ab00b36caca8eee3ffed
Chrysan Angela Piarso <chrysan.angela@gmail.com>
2024-02-15 19:24:10 +0900

    feat: add base client and service functions

--- thanosql/_client.py
+++ thanosql/_client.py
@@ -1,13 +1,12 @@
-from thanosql import API_TOKEN, ENGINE_URL
+from thanosql import API_TOKEN, API_VERSION, ENGINE_URL
 from thanosql._base_client import ThanoSQLBaseClient
 
 
 class ThanoSQL(ThanoSQLBaseClient):
-    api_token: str
-    engine_url: str
-    
-    def __init__(self, api_token: str = API_TOKEN, engine_url: str = ENGINE_URL) -> None:
-        super().__init__()
-        
-        self.api_token = api_token
-        self.engine_url = engine_url
+    def __init__(
+        self,
+        engine_url: str = ENGINE_URL,
+        api_version: str = API_VERSION,
+        api_token: str = API_TOKEN,
+    ) -> None:
+        super().__init__(base_url=engine_url, version=api_version, token=api_token)
