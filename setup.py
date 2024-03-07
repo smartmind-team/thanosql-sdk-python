aa2c3d0b8ab1ccb43c002b730615e37dd19ecb1f
Chrysan Angela Piarso <chrysan.angela@gmail.com>
2024-03-07 14:11:51 +0900

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+version = os.environ.get("THANOSQL_SDK_VERSION")
+
+setup(
+    name="thanosql",
+    version=version,    
+    description="ThanoSQL SDK for Python",
+    url="https://github.com/smartmind-team/thanosql-python",
+    author="SmartMind",
+    author_email="dev@smartmind.team",
+    license="MIT",
+    python_requires=">= 3.10",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="smartmind thanosql sdk",
+    packages=["thanosql"],
+    install_requires=["requests", "pydantic>=2.0"],
+    include_package_data=True,
+    zip_safe=False,
+)
