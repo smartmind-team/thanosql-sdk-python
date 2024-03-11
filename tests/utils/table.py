8f53e214e2de9a26cc3a3b6cd0d16d3e647c448b
Chrysan Angela Piarso <chrysan.angela@gmail.com>
2024-03-11 10:49:42 +0900

    feat: add test schema

--- tests/utils/table.py
+++ tests/utils/table.py
@@ -2,13 +2,34 @@ from thanosql._client import ThanoSQL
 from thanosql.resources import TableObject, TableServiceObject
 
 
-def create_table(client: ThanoSQL, name: str, schema: str | None = None, table: TableObject | None = None) -> TableServiceObject:
+def create_schema(client: ThanoSQL, name: str) -> str:
+    res = client.schema.create(name=name)
+    return res["schema"]
+
+
+def delete_schema(client: ThanoSQL, name: str) -> dict:
+    query = f"DROP SCHEMA {name} CASCADE"
+    return client.query.execute(query=query)
+
+
+def create_table(
+    client: ThanoSQL,
+    name: str,
+    schema: str | None = None,
+    table: TableObject | None = None,
+) -> TableServiceObject:
     res = client.table.create(name=name, schema=schema, table=table)
     res = client.table.get(name=name, schema=schema)
     return res
 
 
-def create_view(client: ThanoSQL, name: str, column_names: str | list[str], table_name: str, schema: str | None = None) -> dict:
+def create_view(
+    client: ThanoSQL,
+    name: str,
+    column_names: str | list[str],
+    table_name: str,
+    schema: str | None = None,
+) -> dict:
     if schema:
         name = f"{schema}.{name}"
 
@@ -17,7 +38,7 @@ def create_view(client: ThanoSQL, name: str, column_names: str | list[str], tabl
     elif isinstance(column_names, list):
         columns_string = ", ".join(column_names)
         query = f"CREATE VIEW {name} AS SELECT {columns_string} FROM {table_name}"
-    
+
     res = client.query.execute(query=query)
     res = client.view.get(name=name, schema=schema)
     return res["view"]
