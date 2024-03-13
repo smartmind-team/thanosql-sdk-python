3824ec2f9e099f25bacaed1818d7e3ba9f1f4019
Chrysan Angela Piarso <chrysan.angela@gmail.com>
2024-03-13 15:17:40 +0900

    refactor: run precommit

--- tests/test_table_template.py
+++ tests/test_table_template.py
@@ -5,7 +5,11 @@ from typing import TYPE_CHECKING
 import pytest
 
 from tests.faker import fake
-from thanosql._error import ThanoSQLAlreadyExistsError, ThanoSQLNotFoundError, ThanoSQLValueError
+from thanosql._error import (
+    ThanoSQLAlreadyExistsError,
+    ThanoSQLNotFoundError,
+    ThanoSQLValueError,
+)
 from thanosql.resources import TableObject
 
 if TYPE_CHECKING:
@@ -17,28 +21,39 @@ latest_version = "2.0"
 def test_create_table_template_invalid(client: ThanoSQL, empty_table_template: dict):
     # check that we cannot create table template with too long of a name
     with pytest.raises(ThanoSQLValueError):
-        client.table.template.create(name=fake.unique.pystr(min_chars=35, max_chars=40).lower(), table_template=TableObject())
-    
+        client.table.template.create(
+            name=fake.unique.pystr(min_chars=35, max_chars=40).lower(),
+            table_template=TableObject(),
+        )
+
     # check that we cannot create table template with invalid characters
     with pytest.raises(ThanoSQLValueError):
         client.table.template.create(name="!#%", table_template=TableObject())
-    
+
     # creation of empty table template version 1.0 is done in conftest set-up
     assert "name" in empty_table_template
     name = empty_table_template["name"]
-    
+
     # check that we cannot create table template with the same name and version
     with pytest.raises(ThanoSQLAlreadyExistsError):
         client.table.template.create(name=name, table_template=TableObject())
-        
+
     # invalid version format should not work
     with pytest.raises(ThanoSQLValueError):
-        client.table.template.create(name=name, table_template=TableObject(), version="2.0.0")
+        client.table.template.create(
+            name=name, table_template=TableObject(), version="2.0.0"
+        )
 
 
-def test_create_table_template_success(client: ThanoSQL, empty_table_template_name: str):    
+def test_create_table_template_success(
+    client: ThanoSQL, empty_table_template_name: str
+):
     # creating the same template with different (valid) version should work
-    res = client.table.template.create(name=empty_table_template_name, table_template=TableObject(), version=latest_version)
+    res = client.table.template.create(
+        name=empty_table_template_name,
+        table_template=TableObject(),
+        version=latest_version,
+    )
     assert {"message", "table_template_name"} == set(res.keys())
 
 
@@ -52,7 +67,7 @@ def test_get_table_template_not_found(client: ThanoSQL, empty_table_template_nam
     # random name that is yet to be used for creation should not work
     with pytest.raises(ThanoSQLNotFoundError):
         client.table.template.get(name=fake.unique.pystr(8))
-        
+
     # random version should not work
     with pytest.raises(ThanoSQLNotFoundError):
         client.table.template.get(name=empty_table_template_name, version="3.0")
@@ -64,13 +79,13 @@ def test_get_table_template(client: ThanoSQL, empty_table_template_name: str):
     assert {"table_templates", "versions"} == set(res.keys())
     assert len(res["table_templates"]) == 1
     assert res["table_templates"][0]["version"] == latest_version
-    
+
     # only the specified version should be returned
     res = client.table.template.get(name=empty_table_template_name, version="1.0")
     assert {"table_templates", "versions"} == set(res.keys())
     assert len(res["table_templates"]) == 1
     assert res["table_templates"][0]["version"] == "1.0"
-    
+
     # without any version in the request, all table templates should be returned
     res = client.table.template.get(name=empty_table_template_name)
     assert {"table_templates", "versions"} == set(res.keys())
@@ -87,34 +102,42 @@ def test_get_table_templates_default(client: ThanoSQL):
     assert len(res["table_templates"]) >= 2
 
 
-def test_delete_table_template_not_found(client: ThanoSQL, empty_table_template_name: str):
+def test_delete_table_template_not_found(
+    client: ThanoSQL, empty_table_template_name: str
+):
     # check that we cannot delete table template with random name
     with pytest.raises(ThanoSQLNotFoundError):
         client.table.template.delete(name=fake.unique.pystr(8))
-        
+
     # random version should also not work
     with pytest.raises(ThanoSQLNotFoundError):
         client.table.template.delete(name=empty_table_template_name, version="3.0")
 
 
-def test_delete_table_template_success(client: ThanoSQL, empty_table_template_name: str):
+def test_delete_table_template_success(
+    client: ThanoSQL, empty_table_template_name: str
+):
     # delete a specific version only
-    res = client.table.template.delete(name=empty_table_template_name, version=latest_version)
+    res = client.table.template.delete(
+        name=empty_table_template_name, version=latest_version
+    )
     assert res["table_template_name"] == empty_table_template_name
-    
+
     # now said version should not be found
     with pytest.raises(ThanoSQLNotFoundError):
-        client.table.template.get(name=empty_table_template_name, version=latest_version)
-    
+        client.table.template.get(
+            name=empty_table_template_name, version=latest_version
+        )
+
     # the other version should still exist
     # no need for comprehensive asserts as it is covered in previous tests already
     res = client.table.template.get(name=empty_table_template_name, version="1.0")
     assert res["versions"][0] == "1.0"
-    
+
     # delete all versions
     res = client.table.template.delete(name=empty_table_template_name)
     assert res["table_template_name"] == empty_table_template_name
-    
+
     # now any table template with this name should not exist
     with pytest.raises(ThanoSQLNotFoundError):
         client.table.template.get(name=empty_table_template_name)
