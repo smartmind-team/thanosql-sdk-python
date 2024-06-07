9fad338de779f1456e85ea84cded3579d44cd88e
Chrysan Angela <56180830+nokchalatte@users.noreply.github.com>
2024-06-07 20:40:16 +0900

    Feat/engine2.0 (#29)
    * feat: change file sdk and tests to fm
    
    * docs: update example
    
    * docs: add note about string xlsx
    
    * feat: update version and upload xlsx docstring

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,134 +1,83 @@
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
 import pytest
 
-from tests.faker import fake
 from thanosql._error import ThanoSQLNotFoundError, ThanoSQLValueError
 
 if TYPE_CHECKING:
     from thanosql._client import ThanoSQL
 
-column_name = "name"
-int_column_name = "price"
-# ideally we should delete this dir after testing
-# but as there is no API for that, it is somewhat difficult and risky
-# so for now we will have a dedicated, fixed folder name for all tests
 dir_name = "test"
 file_name = "file_image.jpeg"
-default_file_path = f"drive/{file_name}"
+relative_file_path = f"{dir_name}/{file_name}"
+temp_file_name = "temp"
 
 
-def test_upload_file(client: ThanoSQL, basic_table_name: str):
-    # dir not subpath of drive/
-    with pytest.raises(ThanoSQLValueError):
-        client.file.upload(path=file_name, dir="..")
+def test_create_folder(client: ThanoSQL):
+    res = client.file.create(dir_name)
+    assert res.content_info.name == dir_name
+    # we will check whether the folder is created successfully again
+    # when we upload a file to it in the next test
 
-    # nonexistent table
+
+def test_upload_file(client: ThanoSQL):
+    # nonexistent file in local
     with pytest.raises(ThanoSQLNotFoundError):
-        client.file.upload(
-            path=file_name,
-            db_commit=True,
-            table=fake.unique.pystr(8),
-            column=column_name,
-            dir=dir_name,
-        )
+        client.file.create(file="random_file")
 
-    # nonexistent column
-    with pytest.raises(ThanoSQLNotFoundError):
-        client.file.upload(
-            path=file_name,
-            db_commit=True,
-            table=basic_table_name,
-            column=fake.unique.pystr(8),
-            dir=dir_name,
-            if_exists="replace",
-        )
+    res = client.file.create(dir_name, file_name)
+    assert res.content_info.path == relative_file_path
 
-    # trying to insert text into integer column
+    # cannot upload to file as destination
     with pytest.raises(ThanoSQLValueError):
-        client.file.upload(
-            path=file_name,
-            db_commit=True,
-            table=basic_table_name,
-            column=int_column_name,
-            dir=dir_name,
-            if_exists="replace",
-        )
-
-    # upload without db_commit and dir
-    # note that we cannot check the existence of uploaded files as it requires user_data_root
-    # which can technically be saved, but it increases complexity and safety risk
-    res = client.file.upload(path=file_name, if_exists="replace")
-    assert res["data"]["file_path"] == default_file_path
-
-    # upload with db_commit and dir
-    target_table = client.table.get(name=basic_table_name)
-    record_count_before = target_table.get_records().total
-
-    res = client.file.upload(
-        path=file_name,
-        db_commit=True,
-        table=basic_table_name,
-        column=column_name,
-        dir=dir_name,
-        if_exists="replace",
-    )
-    assert res["data"]["file_path"] == f"drive/{dir_name}/{file_name}"
-    assert res["data"]["table_name"] == basic_table_name
-    assert res["data"]["column_name"] == column_name
-
-    record_count_after = target_table.get_records().total
-    assert record_count_after == record_count_before + 1
+        client.file.create(relative_file_path, file_name)
 
 
 def test_get_files(client: ThanoSQL):
-    # path must start with drive/
+    # nonexistent file/directory
+    with pytest.raises(ThanoSQLNotFoundError):
+        client.file.get("random_file")
+
+    res = client.file.get(relative_file_path)
+    assert "".join([res.content_info.name, res.content_info.format]) == file_name
+    assert res.content_info.path == relative_file_path
+    assert res.content_info.type == "file"
+
+    res = client.file.get()
+    assert res.content_info.type == "directory"
+    for content in res.content_info.content:
+        if content["name"] == dir_name:
+            return
+    raise AssertionError("Test folder not found in root directory")
+
+
+def test_download_file(client: ThanoSQL):
+    # we cannot download a directory
     with pytest.raises(ThanoSQLValueError):
-        client.file.list(path=dir_name)
+        client.file.get(dir_name, "download")
 
-    res = client.file.list(path=f"drive/{dir_name}/*")
-    # at least the file we just uploaded
-    assert isinstance(res["data"]["matched_pathnames"], list)
-    assert len(res["data"]["matched_pathnames"]) >= 1
+    # upload a dummy file that we can delete later
+    with open(temp_file_name, "w") as f:
+        f.write("Hello ThanoSQL!")
+
+    res = client.file.create("/", temp_file_name)
+    assert res.content_info.path == temp_file_name
+
+    res = client.file.get(temp_file_name, "download")
+    assert os.path.isfile(temp_file_name)
+
+    # cleanup
+    os.remove(temp_file_name)
 
 
-def test_delete_file(client: ThanoSQL, basic_table_name: str):
-    # path must start with drive/
-    with pytest.raises(ThanoSQLValueError):
-        client.file.delete(path=dir_name)
+def test_delete_file(client: ThanoSQL):
+    client.file.delete(dir_name)
+    client.file.delete(temp_file_name)
 
+    # nonexistent file/directory -> we already deleted the folder
+    # make sure deletion deletes all folder contents as well
     with pytest.raises(ThanoSQLNotFoundError):
-        client.file.delete(path=f"drive/{fake.unique.pystr(8)}")
-
-    # delete file inside default drive dir
-    res = client.file.delete(path=default_file_path)
-    assert "message" in res
-
-    # delete file inside custom dir
-    path = f"drive/{dir_name}/{file_name}"
-
-    with pytest.raises(ThanoSQLNotFoundError):
-        client.file.delete(
-            path=path, db_commit=True, table=fake.unique.pystr(8), column=column_name
-        )
-
-    with pytest.raises(ThanoSQLNotFoundError):
-        client.file.delete(
-            path=path,
-            db_commit=True,
-            table=basic_table_name,
-            column=fake.unique.pystr(8),
-        )
-
-    target_table = client.table.get(name=basic_table_name)
-    record_count_before = target_table.get_records().total
-
-    res = client.file.delete(
-        path=path, db_commit=True, table=basic_table_name, column=column_name
-    )
-    assert "message" in res
-
-    record_count_after = target_table.get_records().total
-    assert record_count_after == record_count_before - 1
+        client.file.delete(relative_file_path)
